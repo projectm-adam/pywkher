6e80114fc2070753cdeefdb240ca90087bc0e1dd
xgenadam <adam.merchant@projectmstudio.com>
2017-04-19 10:29:01 +0000

    delete named temp file pdf

--- pywkher/__init__.py
+++ pywkher/__init__.py
@@ -22,10 +22,9 @@ def generate_pdf(html='', url=''):
 
     if html:
         # Save the HTML to a temp file
-        html_file = NamedTemporaryFile(delete=False, suffix='.html')
+        html_file = NamedTemporaryFile(delete=True, suffix='.html')
         html_file.write(bytes(html, encoding='utf-8'))
         html_file.close()
-        html_file.delete()
 
         # wkhtmltopdf
         call_subprocess([wkhtmltopdf_cmd, '-q', html_file.name, pdf_file.name])
